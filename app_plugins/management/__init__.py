c4ed4ac686c7cff724bd428bd7790831d19cbf0a
doug.napoleone <doug.napoleone@eb06085c-f74e-0410-8ae8-c9c8b944c5f3>
2008-08-07 07:00:55 +0000

    update for new signals famework

--- app_plugins/management/__init__.py
+++ app_plugins/management/__init__.py
@@ -1,4 +1,3 @@
-from django.dispatch import dispatcher
 from django.db.models import signals
 from commands.sync_plugins import sync_app_plugins
 from app_plugins import models as sender_app
@@ -6,7 +5,7 @@ from app_plugins import models as sender_app
 def do_sync(*args, **kwdargs):
     sync_app_plugins()
 
-dispatcher.connect(do_sync, sender=sender_app, signal=signals.post_syncdb)
+signals.post_syncdb.connect(do_sync, sender=sender_app)
 
 if __name__ == "__main__":
     sync_app_plugins()
