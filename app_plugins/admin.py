e4a91e97bfa45bbd2343eaae729e5d2e7dfdf520
brosner <brosner@eb06085c-f74e-0410-8ae8-c9c8b944c5f3>
2008-08-27 10:11:42 +0000

    Updated code to work with Django trunk since oldforms bits were removed.

--- app_plugins/admin.py
+++ app_plugins/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from app_plugins.models import PluginPoint, Plugin, UserPluginPreference
+from app_plugins.forms import AdminPluginPointForm, AdminPluginForm
 
 class PluginPointAdmin(admin.ModelAdmin):
     list_display = ('label', 'index', 'registered', 'status')
     list_filter = ('registered', 'status')
+    form = AdminPluginPointForm
 
 class PluginAdmin(admin.ModelAdmin):
     list_display = ('label', 'index', 'registered', 'required', 'status')
     list_filter = ('registered', 'status')
+    form = AdminPluginForm
 
 class UserPluginPreferenceAdmin(admin.ModelAdmin):
     list_display = ('plugin', 'user', 'index', 'visible')
